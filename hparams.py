42de0e218af5652f9222c824144f731b4414ce68
Konrad Mikuła <konrad.mikula@outlook.com>
2021-07-26 21:47:08 +0200

    add Ppolish lang

--- hparams.py
+++ hparams.py
@@ -25,9 +25,9 @@ def create_hparams(hparams_string=None, verbose=False):
         # Data Parameters             #
         ################################
         load_mel_from_disk=False,
-        training_files='filelists/ljs_audio_text_train_filelist.txt',
-        validation_files='filelists/ljs_audio_text_val_filelist.txt',
-        text_cleaners=['english_cleaners'],
+        training_files='kunikowska/filelist.txt',
+        validation_files='kunikowska/filelist_val.txt',
+        text_cleaners=['basic_cleaners'],
 
         ################################
         # Audio Parameters             #
